fe432ec85c94556c467ff050f2613898c8a18c74
farkaspeti <farkas.peti97@gmail.com>
2019-05-20 10:54:16 +0200

    ScheduleDao/DatabaseScheduleDao/ScheduleModel implemented

--- src/main/java/com/codecool/web/dao/ScheduleDao.java
+++ src/main/java/com/codecool/web/dao/ScheduleDao.java
@@ -1,4 +1,15 @@
 package com.codecool.web.dao;
 
+import com.codecool.web.model.Schedule;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ScheduleDao {
+
+    List<Schedule> findAll() throws SQLException;
+    
+    Schedule findById(int schedule_id) throws  SQLException;
+    
+    
 }
