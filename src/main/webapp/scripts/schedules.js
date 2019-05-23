776c3b2f6ecb06db81686f7b5336267885048984
adamKonyari <adam.konyari@gmail.com>
2019-05-23 14:51:23 +0200

    schedules.js added

--- src/main/webapp/scripts/schedules.js
+++ src/main/webapp/scripts/schedules.js
@@ -1,6 +1,17 @@
-function onSchedulesResponse() {
+function createScheduleList(scheduleList) {
+    const ulEl = document.createElement('ul');
+    for (let i = 0; i < scheduleList.length; i++) {
+        const schedule = scheduleList[i];
+        const pEl = document.createElement('p');
+        pEl.textContent = schedule.title;
+        ulEl.appendChild(pEl);
+    }
+    return ulEl;
+}
+
+function onSchedulesReceived() {
     if (this.status === OK) {
         const scheduleList = JSON.parse(this.responseText);
-
+        sideNavContentDivEl.appendChild(createScheduleList(scheduleList));
     }
 }
