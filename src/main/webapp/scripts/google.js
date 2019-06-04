0255cda872b235e119d2c3f8965a07d0fc046ab6
farkaspeti <farkas.peti97@gmail.com>
2019-06-04 14:40:27 +0200

    google aut in progress

---
+++ src/main/webapp/scripts/google.js
@@ -0,0 +1,29 @@
+function onSignIn(googleUser) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'GoogleSingIn');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        console.log('Signed in as: ' + xhr.responseText);
+    }
+    xhr.send('idtoken=' + id_token);
+
+    // Useful data for your client-side scripts:
+    const profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    const id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
